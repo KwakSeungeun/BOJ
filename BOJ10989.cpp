f5459d4441f580ce12f7fbb8028d99d0a2dc723e
KwakSeungeun <kseun0410@gmail.com>
2019-03-06 00:30:59 +0900

    10989 시간초과

---
+++ BOJ10989.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int a[10001] = { 0 };
+
+int main() {
+	int N;
+	cin >> N;
+	int temp;
+	for (int i = 0; i < N; i++) {
+		cin >> temp;
+		a[temp]++;
+	}
+
+	for (int i = 1; i <= 10000; i++) {
+		while (a[i] != 0) {
+			cout << i << endl;
+			a[i]--;
+		}
+	}
+	return 0;
+}
